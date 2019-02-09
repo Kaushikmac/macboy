8cf9476affbf662329be64020e3c2ef0d0a48b3b
Kaushikmac <47468395+Kaushikmac@users.noreply.github.com>
2019-02-09 16:12:02 +0530

    Update positive.c

--- positive.c
+++ positive.c
@@ -1,19 +1,4 @@
-
-Conversation opened. 1 unread message.
-
-Skip to content
-Using Gmail with screen readers
-
-6 of 9,497
-(no subject)
-
-Navaneeth N <n.navaneeth1999@gmail.com>
-Attachments
-Fri, Feb 8, 7:11 PM (20 hours ago)
-to NAVANEETH, me
-
-
-11 Attachments
+#include<stdio.h>
 
 int main()
 {
@@ -24,7 +9,7 @@ int main()
  
    if(a==0){
 
-        printf("equals zero");
+        printf("Zero");
    
  }
     
