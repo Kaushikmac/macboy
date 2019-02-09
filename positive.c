a6bba2ae20d655c0b4d62b5738c6228b6901e9d2
Kaushikmac <47468395+Kaushikmac@users.noreply.github.com>
2019-02-09 16:05:31 +0530

    Create positive.c

---
+++ positive.c
@@ -0,0 +1,52 @@
+
+Conversation opened. 1 unread message.
+
+Skip to content
+Using Gmail with screen readers
+
+6 of 9,497
+(no subject)
+
+Navaneeth N <n.navaneeth1999@gmail.com>
+Attachments
+Fri, Feb 8, 7:11 PM (20 hours ago)
+to NAVANEETH, me
+
+
+11 Attachments
+
+int main()
+{
+
+    int a;
+ 
+   scanf("%d",&a);
+ 
+   if(a==0){
+
+        printf("equals zero");
+   
+ }
+    
+else if(a>0){
+
+        printf("Positive");
+   
+ }
+ 
+   else if(a<0){
+
+        printf("Negative");
+ 
+   }
+ 
+   else {
+
+        printf("not an integer");
+ 
+   }
+
+
+    return 0;
+
+}
